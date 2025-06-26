cdd88ba43585472b0f83825cd1896483b45b26ea
Seth Boehm <20sboehm@gmail.com>
2025-06-26 17:59:22 -0600

    forgot this part

--- MultiplayerController.gd
+++ MultiplayerController.gd
@@ -7,14 +7,30 @@ extends Control
 @onready var start_game: Button = $StartGame
 
 func _ready() -> void:
-	multiplayer.peer_connected.connect(player_connected)
+	multiplayer.peer_connected.connect(peer_connected)
+	multiplayer.peer_disconnected.connect(peer_disconnected)
+	multiplayer.connected_to_server.connect(connected_to_server)
+	multiplayer.connection_failed.connect(connection_failed)
 	
 	host.pressed.connect(_on_host_button_down)
 	join.pressed.connect(_on_join_button_down)
 	start_game.pressed.connect(_on_start_game_button_down)
 
-func player_connected(id: int):
+# Called on the server and clients
+func peer_connected(id: int):
 	print("Player connected: " + str(id))
+
+# Called on the server and clients
+func peer_disconnected(id: int):
+	print("Player disconnected: " + str(id))
+
+# Called only from clients
+func connected_to_server() -> void:
+	print("Connected to server")
+
+# Called only from clients
+func connection_failed() -> void:
+	print("Failed to connect to server")
 
 func _on_host_button_down() -> void:
 	pass
